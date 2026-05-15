2d55d382cf25d6af3fb30abfbe5a46b303a07d36
azuretestcosmoops-hue <azuretestcosmoops@gmail.com>
2026-05-15 11:09:04 +0530

    Added calculator program

---
+++ calculator.py
@@ -0,0 +1,11 @@
+print("WELCOME TO SIMPLE CALCULATOR")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("\nRESULTS:")
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
